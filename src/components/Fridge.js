56e6f278c3676d20978c5b16a2e3962d5588838b
Steve Song <steve.b.song92@gmail.com>
2021-02-04 20:17:07 -0500

    FE & BE: Redux store for ingredients and Fridge page has been created

--- src/components/Fridge.js
+++ src/components/Fridge.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Edit from "./Edit";
+import {
+  addIngredientThunk,
+  deleteIngredientThunk,
+  fetchIngredients,
+} from "../store";
 
-const Fridge = () => {
-  return <div>FRIDGE COMPONENT</div>;
+const Fridge = (props) => {
+  const { getIngredients, deleteIngredient, user, ingredients } = props;
+
+  useEffect(() => {
+    getIngredients(user);
+  }, []);
+
+  return (
+    <>
+      <div>
+        {ingredients.map((singleIngredient) => (
+          <>
+            <div>{singleIngredient}</div>
+            <button
+              onClick={() => {
+                deleteIngredient(user, singleIngredient);
+              }}
+            >
+              delete
+            </button>
+            <Link
+              to={`/fridge/${singleIngredient}/edit`}
+            >
+              edit
+            </Link>
+          </>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Fridge;
+const mapState = (state) => {
+  return {
+    user: state.user,
+    ingredients: state.ingredients,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    getIngredients: (userId) => dispatch(fetchIngredients(userId)),
+    addIngredient: (userId, ingredient) =>
+      dispatch(addIngredientThunk(userId, ingredient)),
+    deleteIngredient: (userId, ingredient) =>
+      dispatch(deleteIngredientThunk(userId, ingredient)),
+  };
+};
+
+export default connect(mapState, mapDispatch)(Fridge);
--- src/store/ingredients.js
+++ src/store/ingredients.js
@@ -1,10 +1,33 @@
+import { IfFirebaseAuthed } from "@react-firebase/auth";
 import { dbService } from "../fbase";
 
 const GET_INGREDIENTS = "GET_INGREDIENTS";
+const EDIT_INGREDIENTS = "EDIT_INGREDIENTS";
+const DELETE_INGREDIENTS = "DELETE_INGREDIENTS";
+const ADD_INGREDIENTS = "ADD_INGREDIENTS";
 
 const getIngredients = (ingredients) => {
   return {
     type: GET_INGREDIENTS,
+    ingredients,
+  };
+};
+const addIngredients = (ingredient) => {
+  return {
+    type: ADD_INGREDIENTS,
+    ingredient,
+  };
+};
+const editIngredients = (ingredients) => {
+  return {
+    type: EDIT_INGREDIENTS,
+    ingredients,
+  };
+};
+
+const deleteIngredients = (ingredients) => {
+  return {
+    type: DELETE_INGREDIENTS,
     ingredients,
   };
 };
@@ -29,11 +52,66 @@ export const fetchIngredients = (userId) => {
   };
 };
 
-export default (state = [], action) => {
+export const addIngredientThunk = (userId, ingredient) => {
+  return async (dispatch) => {
+    try {
+      const str = `${ingredient} has been added`
+      window.confirm(str)
+      const res = await dbService.collection("ingredients").add({
+        name: ingredient,
+        createdAt: Date.now(),
+        creatorId: userId,
+      });
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+};
+export const deleteIngredientThunk = (userId, ingredient) => {
+  return async (dispatch) => {
+    try {
+       await dbService
+        .collection("ingredients")
+        .onSnapshot((snapshot) => {
+          snapshot.docs.filter(
+            (doc) =>
+              doc.data().creatorId === userId && doc.data().name === ingredient
+          ).map(item => {
+            dbService.doc(`ingredients/${item.id}`).delete();
+          })
+        });
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+};
+export const editIngredientThunk = (userId, ingredient, newName) => {
+  return async (dispatch) => {
+    try {
+       await dbService
+        .collection("ingredients")
+        .onSnapshot((snapshot) => {
+          snapshot.docs.filter(
+            (doc) =>
+              doc.data().creatorId === userId && doc.data().name === ingredient
+          ).map(item => {
+            dbService.doc(`ingredients/${item.id}`).update({
+              name: newName
+            });
+          })
+        });
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+};
+let initialState = [];
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
   switch (action.type) {
     case GET_INGREDIENTS:
       return action.ingredients;
     default:
-      return [];
+      return state;
   }
 };
