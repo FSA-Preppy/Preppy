406f7558f247c653c226ecdd291b4fd29ca023a9
Steve Song <steve.b.song92@gmail.com>
2021-02-14 23:57:43 -0500

    FE: Empty component created to display sth when ingredients don't exist

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -14,6 +14,7 @@ import "swiper/swiper.scss";
 import "swiper/components/effect-coverflow/effect-coverflow.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
+import Empty from "./Empty";
 
 SwiperCore.use([Navigation, EffectCoverflow]);
 
@@ -44,83 +45,91 @@ const Recipe = (props) => {
 
   return (
     <>
-      <div className="animation-area">
-        <div className="header-container">
-          <h1 className="title">Recipes</h1>
-        </div>
-
-        <ul className="box-area">
-          <div className="body">
-            <div className="recipes-container">
-              <Swiper
-                effect="coverflow"
-                grabCursor="true"
-                centeredSlides="true"
-                spaceBetween={0}
-                slidesPerView={4}
-                loop="true"
-                coverflowEffect={{
-                  rotate: 25,
-                  stretch: 25,
-                  depth: 200,
-                  modifier: 1,
-                  slideShadows: false,
-                }}
-                breakpoints={{
-                  700: {
-                    spaceBetween: 0,
-                    slidesPerView: 4,
-                  },
-                  500: {
-                    spaceBetween: 100,
-                    slidesPerView: 2,
-                  },
-                  411: {
-                    spaceBetween: 100,
-                    slidesPerView: 2,
-                  },
-                  300: {
-                    spaceBetween: 0,
-                    slidesPerView: 1,
-                  },
-                }}
-              >
-                {recipes.map((recipe, idx) => {
-                  return (
-                    <SwiperSlide key={idx}>
-                      <div className="recipes-slide" key={recipe.id}>
-                        <Card className={classes.root}>
-                          <CardActionArea href={recipe.url} target="_blank">
-                            <CardMedia
-                              className={classes.media}
-                              image={recipe.image}
-                              title={recipe.name}
-                            />
-
-                            <CardContent>
-                              <div className="recipe-name-container">
-                                <div className="recipe-name">{recipe.name}</div>
-                              </div>
-                            </CardContent>
-                          </CardActionArea>
-                        </Card>
-                      </div>
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
+      {recipes.length ? (
+        <>
+          <div className="animation-area">
+            <div className="header-container">
+              <h1 className="title">Recipes</h1>
             </div>
+
+            <ul className="box-area">
+              <div className="body">
+                <div className="recipes-container">
+                  <Swiper
+                    effect="coverflow"
+                    grabCursor="true"
+                    centeredSlides="true"
+                    spaceBetween={0}
+                    slidesPerView={4}
+                    loop="true"
+                    coverflowEffect={{
+                      rotate: 25,
+                      stretch: 25,
+                      depth: 200,
+                      modifier: 1,
+                      slideShadows: false,
+                    }}
+                    breakpoints={{
+                      700: {
+                        spaceBetween: 0,
+                        slidesPerView: 4,
+                      },
+                      500: {
+                        spaceBetween: 100,
+                        slidesPerView: 2,
+                      },
+                      411: {
+                        spaceBetween: 100,
+                        slidesPerView: 2,
+                      },
+                      300: {
+                        spaceBetween: 0,
+                        slidesPerView: 1,
+                      },
+                    }}
+                  >
+                    {recipes.map((recipe, idx) => {
+                      return (
+                        <SwiperSlide key={idx}>
+                          <div className="recipes-slide" key={recipe.id}>
+                            <Card className={classes.root}>
+                              <CardActionArea href={recipe.url} target="_blank">
+                                <CardMedia
+                                  className={classes.media}
+                                  image={recipe.image}
+                                  title={recipe.name}
+                                />
+
+                                <CardContent>
+                                  <div className="recipe-name-container">
+                                    <div className="recipe-name">
+                                      {recipe.name}
+                                    </div>
+                                  </div>
+                                </CardContent>
+                              </CardActionArea>
+                            </Card>
+                          </div>
+                        </SwiperSlide>
+                      );
+                    })}
+                  </Swiper>
+                </div>
+              </div>
+              {/* css animation for background */}
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              {/* css animation for background */}
+            </ul>
           </div>
-          {/* css animation for background */}
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          {/* css animation for background */}
-        </ul>
-      </div>
+        </>
+      ) : (
+        <Empty />
+      )}
     </>
   );
 };
